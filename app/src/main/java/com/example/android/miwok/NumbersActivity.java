e84e12817616701f08ed40412bdf27faec4e38f2
Fernando Fischer <fernandofischer@gmail.com>
2017-04-26 22:52:10 -0300

    Using ListView with ArrayAdapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,59 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        /* Usando array
+        String[] words = new String[10];
+
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
+        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
+        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
+        */
+
+        /* Usando arraylist */
+        ArrayList<String>  words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+/*       LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+
+
+        for (int i = 0; i < words.size(); i++) {
+            
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+
+        }
+*/
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
+
     }
 }

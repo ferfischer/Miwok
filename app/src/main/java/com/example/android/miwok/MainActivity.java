7d04a4b8e1867f581afe694ea35c1d8e6ffb2121
Fernando Fischer <fernandofischer@gmail.com>
2017-06-03 19:02:00 -0300

    Audio focus + selectable items animation

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -81,8 +82,32 @@ public class MainActivity extends AppCompatActivity {
                                          }
                                      }
         );
-
+        Log.v("MainActivity", "onCreate");
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("MainActivity", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("MainActivity", "onDestroy");
+    }
 
 }
